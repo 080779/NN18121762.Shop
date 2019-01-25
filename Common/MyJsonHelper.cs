3c36873cae27c966599ac6f36ce9733b57c980cf
深渊 vz <exceed295@hotmail.com>
2019-01-25 18:51:58 +0800

    3321

---
+++ Common/MyJsonHelper.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace IMS.Common
+{
+    public static class MyJsonHelper
+    {
+        public static object GetFileJson(string path)
+        {
+            string json = string.Empty;
+            string filepath= HttpContext.Current.Server.MapPath(path);
+            using (FileStream fs = new FileStream(filepath, FileMode.Open, System.IO.FileAccess.Read, FileShare.ReadWrite))
+            {
+                using (StreamReader sr = new StreamReader(fs, Encoding.UTF8))
+                {
+                    json = sr.ReadToEnd().ToString();
+                }
+            }
+            return JsonConvert.DeserializeObject(json);
+        }
+    }
+}
---
+++ Web/Controllers/AreaController.cs
@@ -0,0 +1,32 @@
+﻿using IMS.Common;
+using IMS.DTO;
+using IMS.IService;
+using IMS.Web.App_Start.Filter;
+using IMS.Web.Models.TakeCash;
+using IMS.Web.Models.User;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+using System.Web.Script.Serialization;
+
+namespace IMS.Web.Controllers
+{    
+    public class AreaController : ApiController
+    {
+        [AllowAnonymous]
+        [HttpPost]
+        public object Get()
+        {
+            return MyJsonHelper.GetFileJson("~/js/areas.json");
+        }        
+    }    
+}
